dc7bc6f2e96b273b13ad0a51f99aa9b0eb395983
Anderson Almeida <andi.r.almeida@gmail.com>
2019-07-03 00:04:40 -0300

    client folder

---
+++ client/src/components/Mapa.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="mapa"></div>
+</template>
+
+<script>
+export default {
+	name: 'Mapa',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus">
+@import '../assets/stylus/mapa.styl';
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    <Mapa />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Mapa from '@/components/Mapa.vue';
+
+export default {
+	name: 'home',
+	components: {
+		Mapa
+	}
+};
+</script>
+
+<style lang="stylus">
+	@import '../assets/stylus/home.styl';
+</style>
